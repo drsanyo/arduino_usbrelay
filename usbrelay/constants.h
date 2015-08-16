8b2cbe7d252f28bf250bf6dc593911ffadcee963
dr.sanyo <dr.sanyo@gmail.com>
2015-08-16 18:50:16 +0300

    repositoy_init

---
+++ usbrelay/constants.h
@@ -0,0 +1,41 @@
+const byte DATA_PROTOCOL_LENGHT = 2;
+
+const byte PIN_REL_1 = 2;
+const byte PIN_REL_2 = 3;
+const byte PIN_REL_3 = 4;
+const byte PIN_REL_4 = 5;
+const byte PIN_REL_5 = 6;
+const byte PIN_REL_6 = 7;
+const byte PIN_REL_7 = 8;
+const byte PIN_REL_8 = 9;
+
+const long TIMER_HALF_SEC_TIME = 500000;
+const long TIMER_ONE_SEC_TIME = TIMER_HALF_SEC_TIME * 2;
+const int WATCH_DOG_TIME = 10;
+
+const int LF = 10;
+
+const byte ACTION_CODE_INDEX = 0;
+const byte ACTION_ARGS_INDEX = 1;
+
+const byte ACTION_SET_DEVICE_ON = '1';
+const byte ACTION_SET_DEVICE_OFF = '2';
+const byte ACTION_SET_WATCHDOG_STATE = '3';
+const byte ACTION_GET_SYSTEM_STATE = '4';
+
+const byte DEVICE_1 = 0;
+const byte DEVICE_2 = 1;
+const byte DEVICE_3 = 2;
+const byte DEVICE_4 = 3;
+
+const byte MAX_SUPPORTED_DEVICES = 4;
+const byte PINS_TO_DEVICE = 2;
+const byte DEVICES_LST[MAX_SUPPORTED_DEVICES][PINS_TO_DEVICE] = {
+ {PIN_REL_1, PIN_REL_2},
+ {PIN_REL_3, PIN_REL_4},
+ {PIN_REL_5, PIN_REL_6},
+ {PIN_REL_7, PIN_REL_8}
+};
+
+
+const char SET_WATCHDOG_ENABLE = '1'; // all diferent value mean DISABLE
---
+++ usbrelay/usbrelay.ino
@@ -0,0 +1,214 @@
+#include <stdlib.h>
+
+#include "TimerOne.h"
+#include "D:\Development\Arduino\USB Relay\usbrelay\constants.h"
+
+
+char incomingData[DATA_PROTOCOL_LENGHT];
+int watchDogCnt = 0;
+boolean watchdogTimerState = false;
+
+
+/*****************************************************************************************/
+// set device ON
+/*****************************************************************************************/
+void setDeviceOn(int devNr){
+  digitalWrite(DEVICES_LST[devNr][0], LOW);  
+  digitalWrite(DEVICES_LST[devNr][1], LOW);  
+}
+
+/*****************************************************************************************/
+// set device Off
+/*****************************************************************************************/
+void setDeviceOff(int devNr){
+  digitalWrite(DEVICES_LST[devNr][0], HIGH);  
+  digitalWrite(DEVICES_LST[devNr][1], HIGH);  
+}
+
+/*****************************************************************************************/
+// set all devices Off
+/*****************************************************************************************/
+void setAllDevicesOff(){
+  for (byte i = 0; i <= MAX_SUPPORTED_DEVICES; i++){
+    setDeviceOff(i);
+  }
+}
+
+
+/*****************************************************************************************/
+// return system state: pins and wdg timer
+/*****************************************************************************************/
+void getSystemState() {
+  char sysState[6] = "";
+  sysState[0] = digitalRead(PIN_REL_1)+48;8
+  sysState[1] = digitalRead(PIN_REL_2)+48;
+  sysState[2] = digitalRead(PIN_REL_3)+48;
+  sysState[3] = digitalRead(PIN_REL_4)+48;
+  sysState[4] = watchdogTimerState+48;
+  
+  Serial.print("SystemState: ");
+  Serial.println(sysState);
+  
+}
+
+
+/*****************************************************************************************/
+// IN/OUT pins setup and first state init
+/*****************************************************************************************/
+void setupPins() {
+  pinMode(PIN_REL_1, OUTPUT);
+  pinMode(PIN_REL_2, OUTPUT);
+  pinMode(PIN_REL_3, OUTPUT);
+  pinMode(PIN_REL_4, OUTPUT);
+  pinMode(PIN_REL_5, OUTPUT);
+  pinMode(PIN_REL_6, OUTPUT);
+  pinMode(PIN_REL_7, OUTPUT);
+  pinMode(PIN_REL_8, OUTPUT);
+  
+  digitalWrite(PIN_REL_1, HIGH);
+  digitalWrite(PIN_REL_2, HIGH);
+  digitalWrite(PIN_REL_3, HIGH);
+  digitalWrite(PIN_REL_4, HIGH);  
+  digitalWrite(PIN_REL_5, HIGH);
+  digitalWrite(PIN_REL_6, HIGH);
+  digitalWrite(PIN_REL_7, HIGH);
+  digitalWrite(PIN_REL_8, HIGH);  
+}
+
+
+/*****************************************************************************************/
+// Enable watchdog Timer
+/*****************************************************************************************/
+void watchdogTimerEnable() {
+  Timer1.start();
+  watchDogCnt = 0;
+  watchdogTimerState = true;
+}
+
+
+/*****************************************************************************************/
+// Disable watchdog Timer
+/*****************************************************************************************/
+void watchdogTimerDisable() {
+  Timer1.stop();
+  watchDogCnt = 0;
+  watchdogTimerState = false;
+}
+
+
+/*****************************************************************************************/
+// Watchdog timer init
+/*****************************************************************************************/
+void setupWachdogTimer(void (*callbackProc)(), long timerPeriod, boolean startAfterInit) {
+  Timer1.initialize(timerPeriod);
+  Timer1.attachInterrupt(callbackProc);
+  if (startAfterInit == true) {
+    watchdogTimerEnable();
+  } else {
+    watchdogTimerDisable();
+  } 
+}
+
+/*****************************************************************************************/
+// watchdogTimerCallbackProc
+/*****************************************************************************************/
+void watchdogTimerCallbackProc(){
+  ++watchDogCnt;
+  if (watchDogCnt >= WATCH_DOG_TIME){
+    watchDogCnt = 0;
+    setAllDevicesOff();
+  }
+}
+
+
+/*****************************************************************************************/
+// action Set Device to On
+/*****************************************************************************************/
+void actionSetDeviceOn(String devNr){
+  Serial.print("actionSetDeviceOn: ");
+  Serial.println(devNr);
+  setDeviceOn(atoi(devNr.c_str()));
+}
+
+/*****************************************************************************************/
+// action Set Device to Off
+/*****************************************************************************************/
+void actionSetDeviceOff(String devNr){
+  Serial.print("actionSetDeviceOff: ");
+  Serial.println(devNr);
+  setDeviceOff(atoi(devNr.c_str()));
+}
+
+
+
+/*****************************************************************************************/
+// do watchdog timer action
+/*****************************************************************************************/
+void actionSetWatchdogState(char action){
+  Serial.print("actionSetWatchdogState: ");
+  Serial.println(action);
+  
+  if (action == SET_WATCHDOG_ENABLE) {
+    watchdogTimerEnable();
+  } else {
+    watchdogTimerDisable();
+  }
+}
+
+
+/*****************************************************************************************/
+// do get system state action
+/*****************************************************************************************/
+void actionGetSystemState(){
+  Serial.println("actionGetSystemState");
+  getSystemState();
+}
+
+
+/*****************************************************************************************/
+// do an action depending on received char
+/*****************************************************************************************/
+void doAction(char action[]){
+  
+  switch (action[ACTION_CODE_INDEX]) {
+    case ACTION_SET_DEVICE_ON: 
+      actionSetDeviceOn((String)action[ACTION_ARGS_INDEX]); 
+      break;
+    case ACTION_SET_DEVICE_OFF: 
+      actionSetDeviceOff((String)action[ACTION_ARGS_INDEX]); 
+      break;      
+    case ACTION_SET_WATCHDOG_STATE:
+      actionSetWatchdogState(action[ACTION_ARGS_INDEX]);
+      break;
+    case ACTION_GET_SYSTEM_STATE:
+      actionGetSystemState();
+      break;  
+  }
+}
+
+
+/*****************************************************************************************/
+// Setup
+/*****************************************************************************************/
+void setup() {
+  Serial.begin(9600); 
+  
+  setupPins();
+  setupWachdogTimer(
+    watchdogTimerCallbackProc, 
+    TIMER_ONE_SEC_TIME,
+    false);
+}
+
+
+
+/*****************************************************************************************/
+// mani loop
+/*****************************************************************************************/
+void loop() {  
+  if (Serial.available() > 0) {    
+    watchDogCnt = 0;    
+    Serial.readBytesUntil(LF, incomingData, DATA_PROTOCOL_LENGHT);
+    doAction(incomingData);
+  }
+}
